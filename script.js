9113cf617addca465ac4b63db03c2e30ef3548ec
Martha Mamdouh <marthamamdouh5@gmail.com>
2026-07-10 15:36:56 +0200

    Initial Portfolio

---
+++ script.js
@@ -0,0 +1,37 @@
+// Smooth Active Navigation
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+
+// Current Year (لو ضفنا Footer بعدين)
+
+const year = new Date().getFullYear();
+
+console.log("Portfolio Loaded - " + year);
